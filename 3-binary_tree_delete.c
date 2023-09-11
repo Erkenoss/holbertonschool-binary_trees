be98f327710548375408c36003cb7c215291d5e2
Erkenoss <xavier.bertin6@gmail.com>
2023-09-11 18:38:12 +0000

    delete printf test in del function

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -13,8 +13,6 @@ void binary_tree_delete(binary_tree_t *tree)
     if (tree == NULL)
         return;
 
-    printf("suppression de l'élément: %d\n", tree->n);
-
     binary_tree_delete(tree->left);
     binary_tree_delete(tree->right);
 
