bac8934a1594a53a16ae69e104985603e86e1d44
Erkenoss <xavier.bertin6@gmail.com>
2023-09-11 20:40:11 +0000

    add check root

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_is_root - check if a node is the root of the tree
+*
+* @node: node to check
+*
+* Return: 1 if it's the root, else return 0
+*/
+
+
+int binary_tree_is_root(const binary_tree_t *node)
+{
+    if (node == NULL)
+        return (0);
+
+    if (node->parent != NULL)
+        return (0);
+    
+    return (1);
+} 
