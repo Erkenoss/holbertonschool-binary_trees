2ed5399a40e6e677a0c74e678a7ce1f3366dc7b4
Erkenoss <xavier.bertin6@gmail.com>
2023-09-11 19:47:57 +0000

    Add binary_tree_is_leaf

--- binary_trees.h
+++ binary_trees.h
@@ -13,6 +13,7 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
+
 struct binary_tree_s
 {
 	int n;
@@ -31,5 +32,5 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_delete(binary_tree_t *tree);
-
+int binary_tree_is_leaf(const binary_tree_t *node);
 #endif
