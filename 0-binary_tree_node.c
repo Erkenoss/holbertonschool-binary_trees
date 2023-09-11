c7c5361ed015e5e406b6764f32baf438fe553528
Erkenoss <xavier.bertin6@gmail.com>
2023-09-11 13:52:46 +0000

    fixed too much node create

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -13,18 +13,19 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
     binary_tree_t *new_node; 
 
-
-    if (parent == NULL)
-    {
-        parent->left = NULL;
-        parent->right = NULL;
-        parent->parent = NULL;
-        return (parent);
-    }
     new_node = malloc(sizeof(binary_tree_t));
     if (new_node == NULL)
     {
         return (NULL);
+    }
+
+    if (parent == NULL)
+    {
+        new_node->n = value;
+        new_node->left = NULL;
+        new_node->right = NULL;
+        new_node->parent = NULL;
+        return (new_node);
     }
     new_node->n = value;
     new_node->left = NULL;
