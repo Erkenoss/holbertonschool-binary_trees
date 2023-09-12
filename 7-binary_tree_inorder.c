05a06ed6b0fea9d8afc966d8edcf281434920d4a
Erkenoss <xavier.bertin6@gmail.com>
2023-09-12 09:32:01 +0000

    add task 7-

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_preorder - display by the left in first
+*
+* @tree: start to display
+* @func: function pointer to call
+* Return: no return void function
+*/
+
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+    if (tree == NULL)
+        return;
+
+    binary_tree_preorder(tree->left, func);
+    binary_tree_preorder(tree->right, func);
+
+    (func)(tree->n);
+}
