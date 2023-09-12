176e829b02855be5df4593f9ed443a68b114c240
Erkenoss <xavier.bertin6@gmail.com>
2023-09-12 16:43:44 +0000

    add task 10-

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_height - Calcul the deep of a tree
+*
+* @tree: Tree give where check
+*
+* Return: The height of this tree
+*/
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+    size_t depth = 0;
+
+    if (tree == NULL)
+        return (0);
+
+    depth = binary_tree_depth(tree->parent);
+
+    if (tree->parent == NULL)
+        return (0);
+    else
+        return (depth + 1);
+}
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -22,7 +22,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
     count_left = binary_tree_height(tree->left);
     count_right = binary_tree_height(tree->right);
 
-    if (count_left > count_right)
+    if (count_left >= count_right)
         return (count_left + 1);
     else
         return (count_right + 1); 
