d156f2be72f90bdf33fbd99ec63da061c00f5e0a
Erkenoss <xavier.bertin6@gmail.com>
2023-09-12 14:41:20 +0000

    try fixed task 9-

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,17 +10,20 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t count_height = 0;
+    size_t count_left = 0;
+    size_t count_right = 0;
 
     if (tree == NULL)
-        return (count_height);
-    
-    if (tree->left != NULL || tree->right != NULL)
-        if (tree->parent != NULL)
-            count_height++;
-    
-    count_height += binary_tree_height(tree->left);
-    count_height += binary_tree_height(tree->right);
+        return (0);
 
-    return (count_height);
+    if (tree->left == NULL && tree->right == NULL)
+        return (0);
+    
+    count_left = binary_tree_height(tree->left);
+    count_right = binary_tree_height(tree->right);
+
+    if (count_left > count_right)
+        return (count_left + 1);
+    else
+        return (count_right + 1); 
 }
