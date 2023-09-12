dca2519ba21289630201d74dd71d1a6af13d46db
Erkenoss <xavier.bertin6@gmail.com>
2023-09-12 09:23:15 +0000

    add pre-order traversal

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_preorder - display by the left in first
+*
+* @tree: start to display
+*
+* Return: no return void function
+*/
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+    if (tree == NULL)
+        return;
+
+    (func)(tree->n);
+
+    if (tree->left != NULL)
+        binary_tree_preorder(tree->left, func);
+    if (tree->right != NULL)
+        binary_tree_preorder(tree->right, func);
+}
