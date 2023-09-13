f1550fdd5f22bd3cf1b9be6ef9dad37d76505976
Erkenoss <xavier.bertin6@gmail.com>
2023-09-13 15:05:11 +0000

    task 14-

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,51 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_height - Calcul the deep of a tree
+*
+* @tree: Tree give where check
+*
+* Return: The height of this tree
+*/
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+    size_t count_left = 0;
+    size_t count_right = 0;
+
+    if (tree == NULL)
+        return (0);
+    
+    count_left = binary_tree_height(tree->left);
+    count_right = binary_tree_height(tree->right);
+
+    if (count_left >= count_right)
+        return (count_left + 1);
+    else
+        return (count_right + 1); 
+}
+
+/**
+* binary_tree_balance - return the difference betzeen two node
+* at the same height
+*
+* @tree: tree to check
+*
+* Return: the difference between the both
+*/
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+    int left = 0;
+    int right = 0;
+
+    if (tree == NULL)
+    {
+        return (0);
+    }
+
+    left = binary_tree_height(tree->left);
+    right = binary_tree_height(tree->right);
+
+    return (left - right);
+}
