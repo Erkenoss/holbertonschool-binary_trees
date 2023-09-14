f1b7eaf01b3baa43e326f871e837a7a41282ffc8
Erkenoss <xavier.bertin6@gmail.com>
2023-09-14 14:03:44 +0000

    fixed betty style on three files

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -16,7 +16,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left == NULL && tree->right == NULL)
-		return(0);
+		return (0);
 
 	node = binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right);
 
