7be538fca9af6dbfa3d2a0c2769ba4efe6c630a3
Erkenoss <xavier.bertin6@gmail.com>
2023-09-14 13:03:00 +0000

    Add tadk 18-

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_uncle - check which is the uncle
+* of the current node
+*
+* @node: current node
+*
+* Return: pointer on the uncle
+*/
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+    if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+        return (NULL);
+
+    if (node->parent->parent->right != node->parent)
+        return (node->parent->parent->right);
+    return (node->parent->parent->left);
+}
